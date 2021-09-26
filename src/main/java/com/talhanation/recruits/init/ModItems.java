3a520c04481391d55cf0f1684ff3f81cbc6e1690
talhanation <talhakantar@yahoo.com>
2021-09-26 22:39:04 +0200

    re added nomad

--- src/main/java/com/talhanation/recruits/init/ModItems.java
+++ src/main/java/com/talhanation/recruits/init/ModItems.java
@@ -18,7 +18,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> RECRUIT_SPAWN_EGG = RegistryUtils.createSpawnEggItem("recruit", ModEntityTypes.RECRUIT::get, 16755200, 16777045);
     public static final RegistryObject<Item> BOWMAN_SPAWN_EGG = RegistryUtils.createSpawnEggItem("bowman", ModEntityTypes.BOWMAN::get, 16755200, 16777045);
-    //public static final RegistryObject<Item> NOMAD_SPAWN_EGG = RegistryUtils.createSpawnEggItem("nomad", ModEntityTypes.NOMAD::get, 16755200, 16777045);
+    public static final RegistryObject<Item> NOMAD_SPAWN_EGG = RegistryUtils.createSpawnEggItem("nomad", ModEntityTypes.NOMAD::get, 16755200, 16777045);
     public static final RegistryObject<Item> RECRUIT_SHIELD_SPAWN_EGG = RegistryUtils.createSpawnEggItem("recruit_shieldman", ModEntityTypes.RECRUIT_SHIELDMAN::get, 16755200, 16777045);
 
     public static final RegistryObject<BlockItem> RECRUIT_BLOCK = ITEMS.register("recruit_block", () -> new BlockItem(ModBlocks.RECRUIT_BLOCK.get(), (new Item.Properties()).tab(ItemGroup.TAB_DECORATIONS)));
