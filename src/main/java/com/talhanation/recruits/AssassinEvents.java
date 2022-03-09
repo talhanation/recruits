db006c957bffbb0bd7e0d40a6683538a31fd52af
talhanation <talhakantar@yahoo.com>
2022-03-09 18:25:54 +0100

    Update AssassinEvents.java

--- src/main/java/com/talhanation/recruits/AssassinEvents.java
+++ src/main/java/com/talhanation/recruits/AssassinEvents.java
@@ -2,15 +2,16 @@ package com.talhanation.recruits;
 
 import com.talhanation.recruits.entities.AssassinEntity;
 import com.talhanation.recruits.init.ModEntityTypes;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.EntitySpawnPlacementRegistry;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.spawner.WorldEntitySpawner;
 
 public class AssassinEvents {
 
@@ -19,19 +20,17 @@ public class AssassinEvents {
         MinecraftServer server = world.getServer();
         PlayerList list = server.getPlayerList();
         PlayerEntity target = list.getPlayerByName(playerName);
-        double d0 = 0;
-        double d1 = 0;
-        double d2 = 0;
         BlockPos blockPos;
 
         if (target != null) {
             blockPos = calculateSpawnPos(target);
 
-            while (!isFreeToSpawn(blockPos, target)){
+            while (!hasEnoughSpace(world, blockPos)){
                 blockPos = calculateSpawnPos(target);
             }
 
-            if (isFreeToSpawn(blockPos, target)){
+            if (hasEnoughSpace(world, blockPos)){
+                world.setBlock(blockPos, Blocks.REDSTONE_BLOCK.defaultBlockState(), 3);
                 for (int i = 0; i < count; i++) {
                     AssassinEntity assassin = ModEntityTypes.ASSASSIN.get().create(target.level);
                     assassin.setPos(blockPos.getX(), blockPos.getY() + 1, blockPos.getZ());
@@ -46,32 +45,36 @@ public class AssassinEvents {
                 }
             }
         }
-
     }
 
     private static BlockPos calculateSpawnPos(PlayerEntity target){
-        BlockPos blockPos = new BlockPos(target.getX(), target.getY(), target.getZ());
-        double d0 = (double) blockPos.getX() + (target.level.random.nextInt(16) + 32);
-        double d1 = (blockPos.getY() + target.level.random.nextInt(10) - 1);
-        double d2 = (double) blockPos.getZ() + (target.level.random.nextInt(16) + 32);
+        BlockPos blockPos = null;
 
-        System.out.println("DEBUG: d0 = " + d0);
-        System.out.println("DEBUG: d1 = " + d1);
-        System.out.println("DEBUG: d2 = " + d2);
-        return new BlockPos(d0, d1, d2);
-    }
+        for(int i = 0; i < 10; ++i) {
+            int d0 = (int) (target.getX() + (target.level.random.nextInt(16) + 32));
+            int d2 = (int) (target.getZ() + (target.level.random.nextInt(16) + 32));
+            int d1 = target.level.getHeight(Heightmap.Type.WORLD_SURFACE, d0, d2);
 
-    private static boolean isFreeToSpawn(BlockPos blockPos, PlayerEntity targetPlayer){
-        for(int i = 0; i < 10; i++){
-            BlockPos pos = new BlockPos(blockPos.getX(), blockPos.getY() + i, blockPos.getZ());
-            BlockState blockstate = targetPlayer.level.getBlockState(pos);
-            Block block = blockstate.getBlock();
+            System.out.println("DEBUG: x = " + d0);
+            System.out.println("DEBUG: y = " + d1);
+            System.out.println("DEBUG: z = " + d2);
+            BlockPos blockpos1 = new BlockPos(d0, d1, d2);
 
-            if(block.equals(Blocks.AIR)){
-                targetPlayer.level.setBlock(pos, Blocks.REDSTONE_BLOCK.defaultBlockState(), 3);
-                return true;
+            if (WorldEntitySpawner.isSpawnPositionOk(EntitySpawnPlacementRegistry.PlacementType.ON_GROUND, target.level, blockpos1, ModEntityTypes.ASSASSIN.get())) {
+                blockPos = blockpos1;
+                break;
             }
         }
-        return false;
+        return blockPos;
+    }
+
+    private static boolean hasEnoughSpace(IBlockReader reader, BlockPos pos) {
+        for(BlockPos blockpos : BlockPos.betweenClosed(pos, pos.offset(1, 2, 1))) {
+            if (!reader.getBlockState(blockpos).getCollisionShape(reader, blockpos).isEmpty()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
