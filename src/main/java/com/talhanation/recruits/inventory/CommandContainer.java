8ccd04f8c7e031d34d49fb36ab6970c7b616f118
talhanation <talhakantar@yahoo.com>
2022-05-14 10:48:03 +0200

    Update CommandContainer.java

--- src/main/java/com/talhanation/recruits/inventory/CommandContainer.java
+++ src/main/java/com/talhanation/recruits/inventory/CommandContainer.java
@@ -2,20 +2,20 @@ package com.talhanation.recruits.inventory;
 
 import com.talhanation.recruits.Main;
 import de.maxhenkel.corelib.inventory.ContainerBase;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.Inventory;
+import net.minecraft.world.entity.player.Player;
 
 public class CommandContainer extends ContainerBase {
 
 
-    private PlayerEntity playerEntity;
+    private Player playerEntity;
 
-    public CommandContainer(int id, PlayerEntity playerEntity) {
-        super(Main.COMMAND_CONTAINER_TYPE, id, null, new Inventory(0));
-        this.playerEntity = playerEntity;
+    public CommandContainer(int id, Player playerEntity) {
+        super(Main.COMMAND_CONTAINER_TYPE, id, null, null);{
+        //super(Main.COMMAND_CONTAINER_TYPE, id, null, new Inventory(0)); // 1.16 code
+            this.playerEntity = playerEntity;
+        }
     }
-
-    public PlayerEntity getPlayerEntity() {
+    public Player getPlayerEntity() {
         return playerEntity;
     }
 }
