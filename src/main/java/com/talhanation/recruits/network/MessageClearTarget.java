0e0457bdcb5d47f3092b126eafc86a2e86464a4f
talhanation <talhakantar@yahoo.com>
2022-12-26 15:00:18 +0100

    Update MessageClearTarget.java

--- src/main/java/com/talhanation/recruits/network/MessageClearTarget.java
+++ src/main/java/com/talhanation/recruits/network/MessageClearTarget.java
@@ -12,36 +12,37 @@ import java.util.Objects;
 import java.util.UUID;
 
 public class MessageClearTarget implements Message<MessageClearTarget> {
-    private UUID player;
+    private UUID uuid;
     private int group;
 
     public MessageClearTarget(){
     }
 
-    public MessageClearTarget(UUID player, int group) {
-        this.player = player;
+    public MessageClearTarget(UUID uuid, int group) {
+        this.uuid = uuid;
         this.group = group;
+
     }
 
     public Dist getExecutingSide() {
         return Dist.DEDICATED_SERVER;
     }
 
-    public void executeServerSide(NetworkEvent.Context context) {
+    public void executeServerSide(NetworkEvent.Context context){
         List<AbstractRecruitEntity> list = Objects.requireNonNull(context.getSender()).level.getEntitiesOfClass(AbstractRecruitEntity.class, context.getSender().getBoundingBox().inflate(64.0D));
         for (AbstractRecruitEntity recruits : list) {
-            CommandEvents.onStopButton(this.player, recruits, group);
+            CommandEvents.onClearTargetButton(uuid, recruits, group);
         }
     }
     public MessageClearTarget fromBytes(FriendlyByteBuf buf) {
-        this.player = buf.readUUID();
+        this.uuid = buf.readUUID();
         this.group = buf.readInt();
         return this;
     }
 
     public void toBytes(FriendlyByteBuf buf) {
-        buf.writeUUID(this.player);
-        buf.writeInt(this.group);
+        buf.writeUUID(uuid);
+        buf.writeInt(group);
     }
 
 }
