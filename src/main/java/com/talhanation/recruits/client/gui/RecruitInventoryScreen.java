e78e8d2d8e16f0a23601e07d086519bea8566de9
talhanation <talhakantar@yahoo.com>
2023-08-26 16:24:31 +0200

    Update RecruitInventoryScreen.java

--- src/main/java/com/talhanation/recruits/client/gui/RecruitInventoryScreen.java
+++ src/main/java/com/talhanation/recruits/client/gui/RecruitInventoryScreen.java
@@ -16,6 +16,7 @@ import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.animal.camel.Camel;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -306,25 +307,37 @@ public class RecruitInventoryScreen extends ScreenBase<RecruitInventoryMenu> {
         int l = 19;//höhe
 
         //Titles
+
         font.draw(matrixStack, recruit.getDisplayName().getVisualOrderText(), 8, 5, fontColor);
         font.draw(matrixStack, playerInventory.getDisplayName().getVisualOrderText(), 8, this.imageHeight - 96 + 2, fontColor);
 
+        matrixStack.pushPose();
+        matrixStack.scale(0.7F, 0.7F, 1F);
+
+        k = 112;//rechst links
+        l = 32;//höhe
+        int gap = 42;
         //Info
-        font.draw(matrixStack, "Hp:", k, l, fontColor);
-        font.draw(matrixStack, "" + health, k + 25, l , fontColor);
-        font.draw(matrixStack, "Lvl:", k , l  + 10, fontColor);
-        font.draw(matrixStack, "" + recruit.getXpLevel(), k + 25 , l + 10, fontColor);
-        font.draw(matrixStack, "Exp:", k, l + 20, fontColor);
-        font.draw(matrixStack, "" + recruit.getXp(), k + 25, l + 20, fontColor);
-        font.draw(matrixStack, "Kls:", k, l + 30, fontColor);
-        font.draw(matrixStack, ""+ recruit.getKills(), k + 25, l + 30, fontColor);
+
+        font.draw(matrixStack, "Health:", k, l,, fontColor);
+        font.draw(matrixStack, "" + health, k + gap, l, fontColor);
+        font.draw(matrixStack, "Lvl.:", k, l + 10, fontColor);
+        font.draw(matrixStack, "" + recruit.getXpLevel(), k + gap , l + 10, fontColor);
+        font.draw(matrixStack, "Exp.:", k, l + 20, fontColor);
+        font.draw(matrixStack, "" + recruit.getXp(), k + gap, l + 20, fontColor);
+        font.draw(matrixStack, "Kills:", k, l + 30, fontColor);
+        font.draw(matrixStack, ""+ recruit.getKills(), k + gap, l + 30, fontColor);
         font.draw(matrixStack, "Mrl:", k, l + 40, fontColor);
-        font.draw(matrixStack, ""+ moral, k + 25, l + 40, fontColor);
+        font.draw(matrixStack, ""+ moral, k + gap, l + 40, fontColor);
+		font.draw(matrixStack, "Hunger:", k, l + 50, fontColor);
+        font.draw(matrixStack, ""+ hunger, k + gap, l + 50, fontColor);
+        matrixStack.popPose();
         /*
         font.draw(matrixStack, "Moral:", k, l + 30, fontColor);
         font.draw(matrixStack, ""+ recruit.getKills(), k + 25, l + 30, fontColor);
         */
-
+        k = 79;//rechst links
+        l = 19;//höhe
         // command
         String follow = switch (this.follow) {
             case 0 -> TEXT_INFO_WANDER.getString();
