19cc678efc8b7bda0194d44a1a3e76636767de90
talhanation <talhakantar@yahoo.com>
2023-09-19 07:28:22 +0200

    cleanup

--- src/main/java/com/talhanation/recruits/mixin/WalkNodeEvaluatorMixin.java
+++ src/main/java/com/talhanation/recruits/mixin/WalkNodeEvaluatorMixin.java
@@ -1,30 +1,17 @@
 package com.talhanation.recruits.mixin;
 
 import com.talhanation.recruits.entities.AbstractRecruitEntity;
-import com.talhanation.recruits.entities.ai.navigation.RecruitHorsePathNavigation;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
-import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.PathNavigationRegion;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.world.level.pathfinder.Node;
 import net.minecraft.world.level.pathfinder.NodeEvaluator;
 import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
-import net.minecraft.world.phys.AABB;
-import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import javax.annotation.Nullable;
-
 
 @Mixin(WalkNodeEvaluator.class)
 public abstract class WalkNodeEvaluatorMixin extends NodeEvaluator {
@@ -32,6 +19,15 @@ public abstract class WalkNodeEvaluatorMixin extends NodeEvaluator {
     public void horseGetStartNodeWhenRecruitsRides(PathNavigationRegion region, Mob mob, CallbackInfo info){
         if(mob instanceof AbstractHorse horse && horse.isVehicle() && horse.getControllingPassenger() instanceof AbstractRecruitEntity recruit){
             this.entityHeight = Mth.floor(horse.getBbHeight() + 2.0F);
+
+            horse.setPathfindingMalus(BlockPathTypes.WATER, 32.0F);
+            horse.setPathfindingMalus(BlockPathTypes.TRAPDOOR, 32.0F);
+            horse.setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, 32.0F);
+            horse.setPathfindingMalus(BlockPathTypes.DAMAGE_CACTUS, 32.0F);
+            horse.setPathfindingMalus(BlockPathTypes.DANGER_POWDER_SNOW, -1.0F);
+            horse.setPathfindingMalus(BlockPathTypes.DOOR_WOOD_CLOSED, 0.0F);
+            horse.setPathfindingMalus(BlockPathTypes.FENCE, 32.0F);
+            horse.setPathfindingMalus(BlockPathTypes.LAVA, -1.0F);
         }
     }
 
