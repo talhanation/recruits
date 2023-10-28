44715d33a6263d4aca3682ec8fada1f2d96a673a
talhanation <talhakantar@yahoo.com>
2023-10-29 01:27:27 +0200

    improved hunger

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -1169,7 +1169,7 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
 
     public void updateHunger(){
         float currentHunger = getHunger();
-        float newHunger = currentHunger - 7F;
+        float newHunger = currentHunger - 12F;
 
         if(newHunger > 0) {
             setHunger(newHunger);
