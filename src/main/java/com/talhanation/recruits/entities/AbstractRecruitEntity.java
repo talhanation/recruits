e9e2960d2709741b8d0fb5c01f986e688c689f25
talhanation <talhakantar@yahoo.com>
2023-12-12 16:34:02 +0100

    fixed despawn of companions

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -1739,4 +1739,9 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
         }
     }
 
+    @Override
+    public boolean removeWhenFarAway(double p_21542_) {
+        return false;
+    }
+
 }
