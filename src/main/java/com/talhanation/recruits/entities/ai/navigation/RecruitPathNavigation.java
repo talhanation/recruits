b6213b1a084532915d077e0bcc7d890f1a4867be
talhanation <talhakantar@yahoo.com>
2023-12-30 13:02:41 +0100

    fixed recruits get stuck in fences

--- src/main/java/com/talhanation/recruits/entities/ai/navigation/RecruitPathNavigation.java
+++ src/main/java/com/talhanation/recruits/entities/ai/navigation/RecruitPathNavigation.java
@@ -18,7 +18,7 @@ public class RecruitPathNavigation extends GroundPathNavigation {
         recruit.setPathfindingMalus(BlockPathTypes.DAMAGE_CAUTIOUS, 32.0F);
         recruit.setPathfindingMalus(BlockPathTypes.DANGER_POWDER_SNOW, -1.0F);
         recruit.setPathfindingMalus(BlockPathTypes.DOOR_WOOD_CLOSED, 0.0F);
-        recruit.setPathfindingMalus(BlockPathTypes.FENCE, 32.0F);
+        recruit.setPathfindingMalus(BlockPathTypes.FENCE, -1.0F);
         recruit.setPathfindingMalus(BlockPathTypes.LAVA, -1.0F);
     }
 
