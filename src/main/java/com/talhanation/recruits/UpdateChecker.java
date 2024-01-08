d580161e78f60dc39cd1d311d9e670f1ac267ae5
talhanation <talhakantar@yahoo.com>
2024-01-08 23:18:58 +0100

    added config to disable update checker

--- src/main/java/com/talhanation/recruits/UpdateChecker.java
+++ src/main/java/com/talhanation/recruits/UpdateChecker.java
@@ -1,5 +1,7 @@
 package com.talhanation.recruits;
 
+import com.talhanation.recruits.config.RecruitsClientConfig;
+import com.talhanation.recruits.config.RecruitsServerConfig;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.ClickEvent;
@@ -12,47 +14,52 @@ import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.VersionChecker;
+import net.minecraftforge.fml.config.ModConfig;
 
 public class UpdateChecker {
 
     @SubscribeEvent
     public void onPlayerJoinWorld(PlayerEvent.PlayerLoggedInEvent event){
-        VersionChecker.Status status = VersionChecker.getResult((ModList.get().getModContainerById("recruits").get()).getModInfo()).status();
+        if(RecruitsClientConfig.UpdateCheckerClientside.get()){
+            VersionChecker.Status status = VersionChecker.getResult((ModList.get().getModContainerById("recruits").get()).getModInfo()).status();
+            switch (status){
+                case OUTDATED -> {
+                    Player player = event.getPlayer();
+                    if(player != null){
+                        player.sendMessage(new TextComponent("A new version of Villager Recruits is available!").withStyle(ChatFormatting.GOLD), player.getUUID());
 
-        switch (status){
-            case OUTDATED -> {
-                Player player = event.getPlayer();
-                if(player != null){
-                    player.sendMessage(new TextComponent("A new version of Villager Recruits is available!").withStyle(ChatFormatting.GOLD), player.getUUID());
-
-                    MutableComponent link = new TextComponent("Download the update " + ChatFormatting.BLUE + "here").withStyle(ChatFormatting.GREEN);
-                    link.withStyle(link.getStyle().withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.curseforge.com/minecraft/mc-mods/recruits/files")));
-                    player.sendMessage(link, player.getUUID());
+                        MutableComponent link = new TextComponent("Download the update " + ChatFormatting.BLUE + "here").withStyle(ChatFormatting.GREEN);
+                        link.withStyle(link.getStyle().withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.curseforge.com/minecraft/mc-mods/recruits/files")));
+                        player.sendMessage(link, player.getUUID());
+                    }
+                    else{
+                        Main.LOGGER.warn("Villager recruits is outdated!");
+                    }
                 }
-                else{
-                    Main.LOGGER.warn("Villager recruits is outdated!");
-                }
-            }
 
-            case FAILED -> {
-                Main.LOGGER.error("Villager recruits could not check for updates!");
+                case FAILED -> {
+                    Main.LOGGER.error("Villager recruits could not check for updates!");
+                }
             }
         }
     }
 
     @SubscribeEvent
     public void onServerStarted(ServerStartedEvent event){
-        VersionChecker.Status status = VersionChecker.getResult((ModList.get().getModContainerById("recruits").get()).getModInfo()).status();
+        if(RecruitsServerConfig.UpdateCheckerServerside.get()){
+            VersionChecker.Status status = VersionChecker.getResult((ModList.get().getModContainerById("recruits").get()).getModInfo()).status();
 
-        switch (status){
-            case OUTDATED -> {
-                Main.LOGGER.warn("A new version of Villager Recruits is available!");
-                Main.LOGGER.warn("Download the new update here: https://www.curseforge.com/minecraft/mc-mods/recruits/files");
-            }
+            switch (status){
+                case OUTDATED -> {
+                    Main.LOGGER.warn("A new version of Villager Recruits is available!");
+                    Main.LOGGER.warn("Download the new update here: https://www.curseforge.com/minecraft/mc-mods/recruits/files");
+                }
 
-            case FAILED -> {
-                Main.LOGGER.error("Villager recruits could not check for updates!");
+                case FAILED -> {
+                    Main.LOGGER.error("Villager recruits could not check for updates!");
+                }
             }
         }
+
     }
 }
