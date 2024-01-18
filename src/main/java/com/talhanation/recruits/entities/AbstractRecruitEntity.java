f8ac7b7a1c447b8b5b0ef46d5d536e624ecdc4c9
talhanation <talhakantar@yahoo.com>
2024-01-19 00:47:10 +0100

    fix recruits are despawning

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -144,6 +144,8 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
         }
     }
 
+
+
     private void recruitCheckDespawn() {
         Entity entity = this.level.getNearestPlayer(this, -1.0D);
 
@@ -342,7 +344,10 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
     @Override
     public void readAdditionalSaveData(CompoundTag nbt) {
         super.readAdditionalSaveData(nbt);
-        this.despawnTimer = nbt.getInt("despawnTimer");
+
+        if(nbt.contains("despawnTimer")) this.despawnTimer = nbt.getInt("despawnTimer");
+        else this.despawnTimer = -1;//fix
+
         this.setXpLevel(nbt.getInt("Level"));
         this.setState(nbt.getInt("AggroState"));
         this.setFollowState(nbt.getInt("FollowState"));
