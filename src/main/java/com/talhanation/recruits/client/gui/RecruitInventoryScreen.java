04dc9f923298f50b394163bf2c7dba9da369142a
talhanation <talhakantar@yahoo.com>
2024-01-22 09:02:25 +0100

    disabled promote screen

--- src/main/java/com/talhanation/recruits/client/gui/RecruitInventoryScreen.java
+++ src/main/java/com/talhanation/recruits/client/gui/RecruitInventoryScreen.java
@@ -310,7 +310,7 @@ public class RecruitInventoryScreen extends ScreenBase<RecruitInventoryMenu> {
                         this.renderTooltip(poseStack, TOOLTIP_SPECIAL, i, i1);
                     }
             ));
-            promoteButton.active = false || recruit.getXpLevel() >= 3;
+            promoteButton.active = false && recruit.getXpLevel() >= 3;
 
         }
         else {
@@ -323,7 +323,7 @@ public class RecruitInventoryScreen extends ScreenBase<RecruitInventoryMenu> {
                         this.renderTooltip(poseStack, TOOLTIP_PROMOTE, i, i1);
                     }
             ));
-            promoteButton.active = false || recruit.getXpLevel() >= 3;
+            promoteButton.active = false && recruit.getXpLevel() >= 3;
         }
     }
 
