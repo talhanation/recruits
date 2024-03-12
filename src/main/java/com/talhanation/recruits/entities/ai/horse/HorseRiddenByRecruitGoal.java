21461d6124229a95c24fa0ef408bbf7b094cfb35
talhanation <talhakantar@yahoo.com>
2024-03-12 16:53:15 +0100

    Update HorseRiddenByRecruitGoal.java

--- src/main/java/com/talhanation/recruits/entities/ai/horse/HorseRiddenByRecruitGoal.java
+++ src/main/java/com/talhanation/recruits/entities/ai/horse/HorseRiddenByRecruitGoal.java
@@ -1,5 +1,6 @@
 package com.talhanation.recruits.entities.ai.horse;
 
+import com.talhanation.recruits.entities.AbstractLeaderEntity;
 import com.talhanation.recruits.entities.AbstractRecruitEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
@@ -9,6 +10,8 @@ import net.minecraft.world.entity.animal.horse.AbstractHorse;
 public class HorseRiddenByRecruitGoal extends Goal {
 
     public final AbstractHorse horse;
+    public boolean speedApplied;
+    public boolean leaderSlowSpeed;
     public HorseRiddenByRecruitGoal(AbstractHorse horse){
         this.horse = horse;
     }
@@ -20,6 +23,10 @@ public class HorseRiddenByRecruitGoal extends Goal {
     @Override
     public void start() {
         super.start();
+        speedApplied = false;
+    }
+
+    private void applyHorseSpeed() {
         double speed;
         if(this.horse.getPersistentData().contains("oldSpeed")){
             speed = horse.getPersistentData().getDouble("oldSpeed");
@@ -28,8 +35,26 @@ public class HorseRiddenByRecruitGoal extends Goal {
             speed = this.horse.getAttribute(Attributes.MOVEMENT_SPEED).getValue();
             this.horse.getPersistentData().putDouble("oldSpeed", speed);
         }
+
+        if(this.horse.getControllingPassenger() instanceof AbstractLeaderEntity leader){
+            boolean fastPatrolling = leader.getFastPatrolling();
+            double newSpeed = fastPatrolling ? speed : speed * 0.7D;
+            this.horse.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.255  + newSpeed);
+        }
+        else
+            this.horse.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.255  + speed);
+
         if(this.horse instanceof Camel) this.horse.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.120  + speed);
         else this.horse.getAttribute(Attributes.MOVEMENT_SPEED).setBaseValue(0.255  + speed);
+
+    //}
+
+    @Override
+    public void tick() {
+        if(!speedApplied || this.horse.getControllingPassenger() instanceof AbstractLeaderEntity leader && leaderSlowSpeed != leader.getFastPatrolling()){
+            applyHorseSpeed();
+            speedApplied = true;
+        }
     }
 
     @Override
