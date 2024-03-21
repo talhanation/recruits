f640ffdb3b05d101a93aff7e5194682031f07bdd
talhanation <talhakantar@yahoo.com>
2024-03-21 22:41:24 +0100

    fixed horse cant swim

--- src/main/java/com/talhanation/recruits/entities/ai/horse/HorseRiddenByRecruitGoal.java
+++ src/main/java/com/talhanation/recruits/entities/ai/horse/HorseRiddenByRecruitGoal.java
@@ -5,8 +5,6 @@ import com.talhanation.recruits.entities.AbstractRecruitEntity;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
-import net.minecraft.world.level.block.ChestBlock;
-import net.minecraft.world.level.block.entity.ChestBlockEntity;
 
 public class HorseRiddenByRecruitGoal extends Goal {
 
@@ -50,6 +48,11 @@ public class HorseRiddenByRecruitGoal extends Goal {
         if(!speedApplied || this.horse.getControllingPassenger() instanceof AbstractLeaderEntity leader && leaderFastSpeed != leader.getFastPatrolling()){
             applyHorseSpeed();
             speedApplied = true;
+        }
+
+        //apply movement to horse when in water
+        if(this.horse.isInWater()){
+            this.horse.setDeltaMovement(this.horse.getDeltaMovement().add(0, 0.075, 0));
         }
     }
 
