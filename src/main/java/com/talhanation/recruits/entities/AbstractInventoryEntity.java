425fd96a2986db4fbd1659641add53ef7c0b90e4
talhanation <talhakantar@yahoo.com>
2024-04-12 10:27:58 +0200

    Update AbstractInventoryEntity.java
    added compat with corpse mod

--- src/main/java/com/talhanation/recruits/entities/AbstractInventoryEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractInventoryEntity.java
@@ -1,11 +1,10 @@
 package com.talhanation.recruits.entities;
 
+import com.talhanation.recruits.Main;
+import com.talhanation.recruits.compat.Corpse;
 import com.talhanation.recruits.inventory.RecruitSimpleContainer;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.network.syncher.EntityDataAccessor;
-import net.minecraft.network.syncher.EntityDataSerializers;
-import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
@@ -246,6 +245,12 @@ public abstract class AbstractInventoryEntity extends PathfinderMob {
 
     public void die(DamageSource dmg) {
         super.die(dmg);
+
+        if(Main.isCorpseLoaded && !this.getCommandSenderWorld().isClientSide()){
+            Corpse.spawnCorpse(this);
+        }
+        else
+
         if(this.getCommandSenderWorld().getGameRules().getBoolean(GameRules.RULE_DOENTITYDROPS))
             for (int i = 0; i < this.inventory.getContainerSize(); i++)
                 Containers.dropItemStack(this.level, getX(), getY(), getZ(), this.inventory.getItem(i));
