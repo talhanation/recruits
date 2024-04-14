70634c3016b4ec5ae9abe8aba91941f7393f5258
talhanation <talhakantar@yahoo.com>
2024-04-14 22:01:30 +0200

    improved AI performace when pathfinding

--- src/main/java/com/talhanation/recruits/entities/ai/RecruitFollowOwnerGoal.java
+++ src/main/java/com/talhanation/recruits/entities/ai/RecruitFollowOwnerGoal.java
@@ -1,59 +1,82 @@
 package com.talhanation.recruits.entities.ai;
 
 import com.talhanation.recruits.entities.AbstractRecruitEntity;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.Goal;
+import net.minecraft.world.entity.ai.navigation.PathNavigation;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
 
 import java.util.EnumSet;
 
+
 public class RecruitFollowOwnerGoal extends Goal {
     private final AbstractRecruitEntity recruit;
-
+    private LivingEntity owner;
     private final double speedModifier;
-    private final double within;
+    private int timeToRecalcPath;
+    private final float stopDistance;
+    private final float startDistance;
+    private long lastCanUseCheck;
 
-    public RecruitFollowOwnerGoal(AbstractRecruitEntity recruit, double v, double within) {
+    public RecruitFollowOwnerGoal(AbstractRecruitEntity recruit, double speedModifier, float startDistance, float stopDistance) {
         this.recruit = recruit;
-        this.speedModifier = v;
-        this.within = within;
+        this.speedModifier = speedModifier;
+        this.startDistance = 50;
+        this.stopDistance = 10;
     }
 
     public boolean canUse() {
-        if (this.recruit.getOwner() == null) {
-            return false;
+        long i = this.recruit.level.getGameTime();
+        if (i - this.lastCanUseCheck >= 20L) {
+            this.lastCanUseCheck = i;
+            LivingEntity livingentity = this.recruit.getOwner();
+            LivingEntity target = this.recruit.getTarget();
+            double start = target == null ? startDistance : startDistance + 100;
+            if (livingentity == null) {
+                return false;
+            }
+            else if (livingentity.isSpectator()) {
+                return false;
+            }
+            else if (this.recruit.distanceToSqr(livingentity) < start) {
+                return false;
+            }
+            else {
+                this.owner = livingentity;
+                return this.recruit.getShouldFollow() && !recruit.getFleeing() && recruit.getFollowState() == 1 && !recruit.needsToGetFood() && !recruit.getShouldMount() && !recruit.getShouldMovePos();
+            }
         }
-        else
-            return this.recruit.getShouldFollow() && !recruit.getFleeing() && !recruit.needsToGetFood() && !recruit.getShouldMount();
+        return false;
+
     }
 
     public boolean canContinueToUse() {
-        return canUse();
+        if (this.recruit.getNavigation().isDone()) {
+            return false;
+        }
+        return !(this.recruit.distanceToSqr(this.owner) <= this.stopDistance) && this.recruit.getShouldFollow() && !recruit.getFleeing() && recruit.getFollowState() == 1 && !recruit.needsToGetFood() && !recruit.getShouldMount() && !recruit.getShouldMovePos();
+    }
+
+    public void start() {
+        this.timeToRecalcPath = 0;
+        this.recruit.setIsFollowing(true);
+    }
+
+    public void stop() {
+        this.owner = null;
+        this.recruit.setIsFollowing(false);
+        this.recruit.getNavigation().stop();
     }
 
     public void tick() {
-        if (this.recruit.getOwner() != null){
-            double distance = recruit.getOwner().distanceToSqr(recruit);
-            if(recruit.getTarget() != null){
-                follow(distance, 3, true);
-            }
-            else {
-                follow(distance, 1, false);
-            }
 
-        }
-    }
+        if (--this.timeToRecalcPath <= 0) {
+            this.timeToRecalcPath = this.recruit.getVehicle() != null ? this.adjustedTickDelay(5) : this.adjustedTickDelay(10);
 
-    private void follow(double distance, double multiplier, boolean target){
-        if(distance > within * multiplier) {
-            this.recruit.setIsFollowing(true);
-            this.recruit.getNavigation().moveTo(recruit.getOwner().getX(), recruit.getOwner().getY(), recruit.getOwner().getZ(), this.speedModifier);
-            if (recruit.horizontalCollision || recruit.minorHorizontalCollision) {
-                this.recruit.getJumpControl().jump();
-            }
-        }
-        else{
-            if(!target) this.recruit.getNavigation().stop();
-            this.recruit.setIsFollowing(false);
+            this.recruit.getLookControl().setLookAt(this.owner, 10.0F, (float)this.recruit.getMaxHeadXRot());
+            this.recruit.getNavigation().moveTo(this.owner, this.speedModifier);
         }
     }
 }
-
