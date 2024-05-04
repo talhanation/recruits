41b4248a430218495ae669ae7c2920e4ade71609
talhanation <talhakantar@yahoo.com>
2024-05-04 12:37:17 +0200

    fixed render performance

--- src/main/java/com/talhanation/recruits/client/render/layer/RecruitHumanTeamColorLayer.java
+++ src/main/java/com/talhanation/recruits/client/render/layer/RecruitHumanTeamColorLayer.java
@@ -15,8 +15,34 @@ import net.minecraft.resources.ResourceLocation;
 
 public class RecruitHumanTeamColorLayer extends RenderLayer<AbstractRecruitEntity, PlayerModel<AbstractRecruitEntity>> {
 
-    private static final ResourceLocation LOCATION = new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_cloth.png");
-    private static final ResourceLocation LOCATION2 = new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_base_cloth.png");
+    private static final ResourceLocation[] TEXTURE = {
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_white.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_black.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_grey.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_grey.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_grey.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_blue.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_blue.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_blue.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_green.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_green.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_green.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_red.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_red.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_red.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_brown.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_brown.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_brown.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_light_cyan.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_cyan.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_dark_cyan.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_yellow.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_orange.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_magenta.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_purple.png"),
+            new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_team_gold.png")
+    };
+    private static final ResourceLocation TEXTURE2 = new ResourceLocation(Main.MOD_ID,"textures/entity/human/human_base_cloth.png");
 
     public RecruitHumanTeamColorLayer(LivingEntityRenderer<AbstractRecruitEntity, PlayerModel<AbstractRecruitEntity>> renderer) {
         super(renderer);
@@ -24,13 +50,11 @@ public class RecruitHumanTeamColorLayer extends RenderLayer<AbstractRecruitEntit
 
     public void render(PoseStack poseStack, MultiBufferSource bufferSource, int p_117722_, AbstractRecruitEntity recruit, float p_117724_, float p_117725_, float p_117726_, float p_117727_, float p_117728_, float p_117729_) {
         if(!recruit.isInvisible()){
-            if (recruit.isOwned() && recruit.getTeam() != null) {
-                float[] afloat = recruit.getDyeColor().getTextureDiffuseColors();
-                renderColoredCutoutModel(this.getParentModel(), LOCATION, poseStack, bufferSource, p_117722_, recruit, afloat[0], afloat[1], afloat[2]);
+            if (recruit.getTeam() != null) {
+                this.getParentModel().renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutout(TEXTURE[recruit.getColor()])), p_117722_, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
             }
             else{
-                VertexConsumer vertexconsumer = bufferSource.getBuffer(RenderType.entityCutoutNoCull(LOCATION2));
-                this.getParentModel().renderToBuffer(poseStack, vertexconsumer, p_117722_, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+                this.getParentModel().renderToBuffer(poseStack, bufferSource.getBuffer(RenderType.entityCutout(TEXTURE2)), p_117722_, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
             }
         }
     }
