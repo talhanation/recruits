6c5dc75703d753b25c697d98776414c256131cdc
talhanation <talhakantar@yahoo.com>
2024-06-03 13:04:39 +0200

    fixed hunger bug with other mods

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -1245,8 +1245,9 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
             if(currentMorale < 65) newMorale += 2F;
         }
 
-        if(newMorale > 0) setMoral(newMorale);
-        else setMoral(0F);
+        if(newMorale > 0) newMorale = 0;
+
+        this.setMoral(newMorale);
     }
 
     public void applyMoralEffects(){
@@ -1279,14 +1280,18 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
     }
 
     public void updateHunger(){
-        float currentHunger = getHunger();
-        float newHunger = currentHunger - 12F;
+        float hunger = getHunger();
 
-        if(newHunger > 0) {
-            setHunger(newHunger);
-        }else
-            setHunger(0);
+        if (this.getFollowState() == 2) {
+            hunger -= 2/60F;
+        }
+        else{
+            hunger -= 3/60F;
+        }
 
+        if (hunger < 0) hunger = 0;
+
+        this.setHunger(hunger);
         this.updateMoral();
     }
 
@@ -1308,8 +1313,6 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
     }
 
     public boolean needsToEat(){
-        setHunger((getHunger() - 0.0001F));
-
         if (getHunger() <= 50F){
             return true;
         }
