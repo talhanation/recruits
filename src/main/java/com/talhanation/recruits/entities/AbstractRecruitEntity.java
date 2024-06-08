68d0b01264a610b6617454111704629accf65aac
talhanation <talhakantar@yahoo.com>
2024-06-08 17:32:37 +0200

    fixed old world would not apply variant and biome byte

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -397,7 +397,7 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
         this.setMountTimer(nbt.getInt("mountTimer"));
         this.setUpkeepTimer(nbt.getInt("UpkeepTimer"));
         this.setColor(nbt.getByte("Color"));
-        this.setBiome(nbt.getByte("Biome"));
+
         this.setMaxFallDistance(nbt.getInt("MaxFallDistance"));
 
         if (nbt.contains("HoldPosX") && nbt.contains("HoldPosY") && nbt.contains("HoldPosZ")) {
@@ -442,6 +442,9 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
                     nbt.getInt("UpkeepPosY"),
                     nbt.getInt("UpkeepPosZ")));
         }
+
+        if(nbt.contains("Biome"))this.setBiome(nbt.getByte("Biome"));
+        else applyBiomeAndVariant(this);;
     }
 
     ////////////////////////////////////GET////////////////////////////////////
@@ -932,6 +935,10 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
         recruit.setListen(true);
         recruit.setXpLevel(1);
 
+        applyBiomeAndVariant(recruit);
+    }
+
+    public static void applyBiomeAndVariant(AbstractRecruitEntity recruit){
         //ForgeBiomeTagsProvider
         Holder<Biome> biome = recruit.getCommandSenderWorld().getBiome(recruit.getOnPos());
         byte biomeByte = 2; //PLAINS
@@ -970,8 +977,6 @@ public abstract class AbstractRecruitEntity extends AbstractInventoryEntity{
         recruit.setBiome(biomeByte);
         recruit.setVariant(variant);
     }
-
-
 
     ////////////////////////////////////is FUNCTIONS////////////////////////////////////
 
