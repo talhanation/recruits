1dafd2ae324aa00fc3ddb980f95db4f1599f78f4
talhanation <talhakantar@yahoo.com>
2024-06-14 10:54:53 +0200

    fixed recruits can do strategic fire with no arrows (if config selected)

--- src/main/java/com/talhanation/recruits/entities/AbstractLeaderEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractLeaderEntity.java
@@ -16,7 +16,6 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
