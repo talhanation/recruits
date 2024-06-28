5e31b76f81fa9db4c9e26dade1f1682f12f3d2bf
talhanation <talhakantar@yahoo.com>
2024-06-28 18:03:10 +0200

    wip formation

--- src/main/java/com/talhanation/recruits/CommandEvents.java
+++ src/main/java/com/talhanation/recruits/CommandEvents.java
@@ -16,7 +16,9 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.Mth;
 import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -24,8 +26,10 @@ import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.network.NetworkHooks;
@@ -488,5 +492,58 @@ public class CommandEvents {
         }
 
         return loyals.size();
+    }
+
+    public static void onFormationButton(ServerPlayer player, List<AbstractRecruitEntity> recruits) {
+        Vec3 playerPos = player.position();
+        Vec3 forward = player.getForward();
+        Vec3 left = new Vec3(-forward.z, forward.y, forward.x);
+
+        int maxInRow = 15;
+        double spacing = 2.0;
+
+        List<Vec3> possiblePositions = new ArrayList<>();
+
+        for (int i = 0; i < recruits.size(); i++) {
+            int row = i / maxInRow;
+            int positionInRow = i % maxInRow;
+
+            Vec3 basePos = playerPos.add(forward.scale(-3 * row));
+            Vec3 offset = left.scale((maxInRow / 2F - positionInRow) * spacing);
+
+            Vec3 recruitPos = basePos.add(offset);
+            possiblePositions.add(recruitPos);
+        }
+
+        //recruits.sort(Comparator.comparingDouble(recruit -> recruit.position().distanceTo(playerPos)));
+
+        for (AbstractRecruitEntity recruit : recruits) {
+            if (possiblePositions.isEmpty()) {
+                break;
+            }
+
+            double closestDistance = Double.MAX_VALUE;
+
+            float diff = Mth.degreesDifference(recruit.ownerRot, player.getYRot());
+
+            int m = 0;
+            if (Math.abs(diff) > 90F) {
+                for (int i = 0; i < possiblePositions.size(); i++) {
+                    Vec3 pos = possiblePositions.get(i);
+                    double distance = recruit.position().distanceTo(pos);
+                    if (distance < closestDistance) {
+                        closestDistance = distance;
+                        m = i;
+                    }
+                }
+            }
+            Vec3 dest = possiblePositions.remove(m);
+            BlockPos blockPos = player.getCommandSenderWorld().getHeightmapPos(Heightmap.Types.WORLD_SURFACE, new BlockPos((int) dest.x, (int) dest.y, (int) dest.z));
+
+            recruit.setHoldPos(new Vec3(dest.x, blockPos.getY(), dest.z));
+            recruit.ownerRot = player.getYRot();
+            recruit.formationPos = 1;
+            recruit.setFollowState(3);
+        }
     }
 }
