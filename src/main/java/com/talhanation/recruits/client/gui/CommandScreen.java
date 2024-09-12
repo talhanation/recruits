27b9f5d565e90a22ce1c089bb3de05739228644d
talhanation <talhakantar@yahoo.com>
2024-09-12 11:15:55 +0200

    fix all groups mount

--- src/main/java/com/talhanation/recruits/client/gui/CommandScreen.java
+++ src/main/java/com/talhanation/recruits/client/gui/CommandScreen.java
@@ -663,8 +663,10 @@ public class CommandScreen extends ScreenBase<CommandMenu> {
                         button -> {
                             if (rayEntity != null && !groups.isEmpty()) {
                                 for(RecruitsGroup group : groups) {
-                                    Main.SIMPLE_CHANNEL.sendToServer(new MessageProtectEntity(player.getUUID(), rayEntity.getUUID(), group.getId()));
-                                    Main.SIMPLE_CHANNEL.sendToServer(new MessageMovement(player.getUUID(), 5, formation.getIndex(), group.getId()));
+                                    if (!group.isDisabled()) {
+                                        Main.SIMPLE_CHANNEL.sendToServer(new MessageProtectEntity(player.getUUID(), rayEntity.getUUID(), group.getId()));
+                                        Main.SIMPLE_CHANNEL.sendToServer(new MessageMovement(player.getUUID(), 5, formation.getIndex(), group.getId()));
+                                    }
                                 }
                                 this.sendCommandInChat(5);
                             }
@@ -680,7 +682,9 @@ public class CommandScreen extends ScreenBase<CommandMenu> {
                         button -> {
                             if (rayEntity != null && !groups.isEmpty()) {
                                 for (RecruitsGroup group : groups) {
-                                    Main.SIMPLE_CHANNEL.sendToServer(new MessageMountEntity(player.getUUID(), rayEntity.getUUID(), group.getId()));
+                                    if (!group.isDisabled()) {
+                                        Main.SIMPLE_CHANNEL.sendToServer(new MessageMountEntity(player.getUUID(), rayEntity.getUUID(), group.getId()));
+                                    }
                                 }
                                 this.sendCommandInChat(99);
                             }
