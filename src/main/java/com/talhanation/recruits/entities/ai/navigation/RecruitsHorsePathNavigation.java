e3bac1c6e79a4c0610c4c211c240dff259815be0
talhanation <talhakantar@yahoo.com>
2024-09-28 16:39:47 +0200

    fixed pathfing

--- src/main/java/com/talhanation/recruits/entities/ai/navigation/RecruitsHorsePathNavigation.java
+++ src/main/java/com/talhanation/recruits/entities/ai/navigation/RecruitsHorsePathNavigation.java
@@ -1,10 +1,8 @@
 package com.talhanation.recruits.entities.ai.navigation;
 
-import com.talhanation.recruits.entities.AbstractRecruitEntity;
 import net.minecraft.world.entity.ai.navigation.GroundPathNavigation;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.level.pathfinder.PathFinder;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,15 +11,6 @@ public class RecruitsHorsePathNavigation extends GroundPathNavigation {
     public RecruitsHorsePathNavigation(AbstractHorse horse, Level world) {
         super(horse, world);
         this.horse = horse;
-        horse.setPathfindingMalus(BlockPathTypes.WATER, -1.0F);
-        horse.setPathfindingMalus(BlockPathTypes.TRAPDOOR, -1.0F);
-        horse.setPathfindingMalus(BlockPathTypes.DAMAGE_FIRE, 32.0F);
-        horse.setPathfindingMalus(BlockPathTypes.DAMAGE_CACTUS, 32.0F);
-        horse.setPathfindingMalus(BlockPathTypes.DANGER_POWDER_SNOW, -1.0F);
-        horse.setPathfindingMalus(BlockPathTypes.DOOR_WOOD_CLOSED, 0.0F);
-        horse.setPathfindingMalus(BlockPathTypes.FENCE, -1.0F);
-        horse.setPathfindingMalus(BlockPathTypes.LAVA, -1.0F);
-        horse.setPathfindingMalus(BlockPathTypes.LEAVES, -1.0F);
     }
 
     protected @NotNull PathFinder createPathFinder(int range) {
