5d6079e752c26b645ae7c321e1e1a2bebc75a06e
talhanation <talhakantar@yahoo.com>
2024-10-05 17:00:53 +0200

    Update SailorNodeEvaluator.java

--- src/main/java/com/talhanation/recruits/entities/ai/navigation/SailorNodeEvaluator.java
+++ src/main/java/com/talhanation/recruits/entities/ai/navigation/SailorNodeEvaluator.java
@@ -48,11 +48,12 @@ public class SailorNodeEvaluator extends SwimNodeEvaluator {
 
     @Nullable
     protected Node getNode(int x, int y, int z) {
-        Node node = this.getNodeRaw(x, y, z);
+        Node node = null;
         BlockPathTypes blockpathtypes = this.getCachedBlockType(x, y, z);
         if (blockpathtypes == BlockPathTypes.WATER || blockpathtypes == BlockPathTypes.BREACH) {
             float f = this.mob.getPathfindingMalus(blockpathtypes);
             if (f >= 0.0F) {
+                node = this.getNodeRaw(x, y, z);
                 if (node != null) {
                     node.type = blockpathtypes;
                     node.costMalus = Math.max(node.costMalus, f);
