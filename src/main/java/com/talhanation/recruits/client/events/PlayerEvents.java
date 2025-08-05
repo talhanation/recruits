32328ac5fa9cc0a7889df777a4a51466c6d34c9f
talhanation <talhakantar@yahoo.com>
2025-08-05 10:24:57 +0200

    Update PlayerEvents.java

--- src/main/java/com/talhanation/recruits/client/events/PlayerEvents.java
+++ src/main/java/com/talhanation/recruits/client/events/PlayerEvents.java
@@ -2,8 +2,10 @@ package com.talhanation.recruits.client.events;
 
 import com.talhanation.recruits.client.ClientManager;
 import com.talhanation.recruits.client.gui.overlay.FactionClaimBannerOverlay;
+import com.talhanation.recruits.client.gui.overlay.FactionClaimSiegeOverlay;
 import com.talhanation.recruits.world.RecruitsClaim;
 import com.talhanation.recruits.world.RecruitsClaimManager;
+import com.talhanation.recruits.world.RecruitsTeam;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ChunkPos;
@@ -15,10 +17,11 @@ public class PlayerEvents {
 
     private static ChunkPos lastPlayerChunk = null;
     private static RecruitsClaim currentClaim = null;
+
     @SubscribeEvent
     public void onClientTick(TickEvent.ClientTickEvent event) {
         Player player = Minecraft.getInstance().player;
-        if(player == null) return;
+        if (player == null || event.phase != TickEvent.Phase.END) return;
 
         ChunkPos currentChunk = player.chunkPosition();
 
@@ -27,20 +30,31 @@ public class PlayerEvents {
 
             RecruitsClaim claim = RecruitsClaimManager.getClaimAt(currentChunk, ClientManager.recruitsClaims);
             if (claim != null) {
-                if(currentClaim != null && currentClaim.equals(claim)) return;
-
+                if (currentClaim != null && currentClaim.equals(claim)) return;
                 currentClaim = claim;
-                FactionClaimBannerOverlay.display(claim.getOwnerFaction(), claim.getName());
+
+                if (claim.isUnderSiege) {
+                    /*
+                    RecruitsTeam defender = claim.getOwnerFaction();
+                    RecruitsTeam attacker = claim.attackingParties.get(0);
+
+                    float healthRatio = (float) claim.getHealth() / (float) claim.getMaxHealth();
+
+                    FactionClaimSiegeOverlay.display(claim.getName(), defender, attacker, healthRatio);
+
+                     */
+                } else {
+                    FactionClaimBannerOverlay.display(claim.getOwnerFaction(), claim.getName());
+                }
+            } else {
+                currentClaim = null;
             }
-            else currentClaim = null;
         }
     }
 
     @SubscribeEvent
     public void onRenderGui(RenderGuiOverlayEvent event) {
         FactionClaimBannerOverlay.renderOverlay(event.getGuiGraphics(), event.getWindow().getGuiScaledWidth());
+        //FactionClaimSiegeOverlay.renderOverlay(event.getGuiGraphics(), event.getWindow().getGuiScaledWidth());
     }
-
-
-
 }
