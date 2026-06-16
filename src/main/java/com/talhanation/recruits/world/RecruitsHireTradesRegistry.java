ffd028c60a536565d8fe0c034776d4d059bafa20
talhanation <talhakantar@yahoo.com>
2026-06-16 10:48:25 +0200

    added siege engineer

--- src/main/java/com/talhanation/recruits/world/RecruitsHireTradesRegistry.java
+++ src/main/java/com/talhanation/recruits/world/RecruitsHireTradesRegistry.java
@@ -19,6 +19,7 @@ public class RecruitsHireTradesRegistry {
     private static final Component TITLE_SCOUT = Component.translatable("description.recruits.title.scout");
     private static final Component TITLE_CAPTAIN = Component.translatable("description.recruits.title.captain");
     private static final Component TITLE_PATROL_LEADER = Component.translatable("description.recruits.title.commander");
+    private static final Component TITLE_SIEGE_ENGINEER = Component.translatable("description.recruits.title.siege_engineer");
 
     private static final Component DESCRIPTION_RECRUIT = Component.translatable("description.recruits.recruit");
     private static final Component DESCRIPTION_SHIELDMAN = Component.translatable("description.recruits.shieldman");
@@ -30,7 +31,7 @@ public class RecruitsHireTradesRegistry {
     private static final Component DESCRIPTION_SCOUT = Component.translatable("description.recruits.scout");
     private static final Component DESCRIPTION_CAPTAIN = Component.translatable("description.recruits.captain");
     private static final Component DESCRIPTION_PATROL_LEADER = Component.translatable("description.recruits.commander");
-
+    private static final Component DESCRIPTION_SIEGE_ENGINEER = Component.translatable("description.recruits.siege_engineer");
 
 
     public static RecruitsHireTrade RECRUIT = new RecruitsHireTrade(ModEntityTypes.RECRUIT.getId(), RecruitsServerConfig.RecruitCost.get(), TITLE_RECRUIT, DESCRIPTION_RECRUIT);
@@ -47,6 +48,9 @@ public class RecruitsHireTradesRegistry {
     public static RecruitsHireTrade CAPTAIN = new RecruitsHireTrade(ModEntityTypes.CAPTAIN.getId(), 64, TITLE_CAPTAIN, DESCRIPTION_CAPTAIN);
     public static RecruitsHireTrade PATROL_LEADER = new RecruitsHireTrade(ModEntityTypes.PATROL_LEADER.getId(), 64, TITLE_PATROL_LEADER, DESCRIPTION_PATROL_LEADER);
 
+    public static RecruitsHireTrade SIEGE_ENGINEER = new RecruitsHireTrade(ModEntityTypes.SIEGE_ENGINEER.getId(), 48, TITLE_SIEGE_ENGINEER, DESCRIPTION_SIEGE_ENGINEER);
+
+
     private static final Map<String, Map<Integer, List<RecruitsHireTrade>>> TRADES = new HashMap<>();
     public static void registerTrades() {
         // === Trader Type 0 ===
@@ -54,28 +58,20 @@ public class RecruitsHireTradesRegistry {
         addTrade("infantry", 2, HORSEMAN);
         addTrade("infantry", 3, CAPTAIN, PATROL_LEADER);
 
-        // === Trader Type 1 ===
+         // === Trader Type 1 ===
+        addTrade("infantry2", 1, RECRUIT, SHIELDMAN);
+        addTrade("infantry2", 2, SCOUT);
+        addTrade("infantry2", 3, SIEGE_ENGINEER, MESSENGER);
+
+        // === Trader Type 2 ===
         addTrade("ranged", 1, RECRUIT, BOWMAN);
         addTrade("ranged", 2, NOMAD);
         addTrade("ranged", 3, MESSENGER, SCOUT);
 
-        // === Trader Type 2 ===
+        // === Trader Type 3 ===
         addTrade("ranged2", 1, RECRUIT, CROSSBOWMAN);
         addTrade("ranged2", 2, SHIELDMAN);
         addTrade("ranged2", 3, MESSENGER, SCOUT);
-
-        /*
-        // === Trader Type 3 (Workers Addon) ===
-        addTrade("workers", 1, "miner", "lumberjack");
-        addTrade("workers", 2, "builder");
-        addTrade("workers", 3, "recruit");
-
-        // === Trader Type 4 (Workers Addon) ===
-        addTrade("workers2", 1, "farmer", "lumberjack");
-        addTrade("workers2", 2, "miner");
-        addTrade("workers2", 3, "recruit");
-        */
-    }
 
     public static void addTrade(String traderType, int level, RecruitsHireTrade... units) {
         TRADES.computeIfAbsent(traderType, k -> new HashMap<>())
