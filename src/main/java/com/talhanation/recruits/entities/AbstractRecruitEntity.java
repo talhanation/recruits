12f09dd5f3733a9ad7d758edb41f99cc54c23c59
talhanation <talhakantar@yahoo.com>
2026-06-25 13:33:59 +0200

    Update AbstractRecruitEntity.java

--- src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
+++ src/main/java/com/talhanation/recruits/entities/AbstractRecruitEntity.java
@@ -32,6 +32,7 @@ import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvent;
